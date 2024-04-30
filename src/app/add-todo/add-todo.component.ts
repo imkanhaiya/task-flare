0942c66f9d722e89f85a334c602037d1a1b8f275
KANHAIYA JI <kanhaiyaji14@gmail.com>
2024-04-30 19:15:19 +0530

    Use enum to restrict type of values status field can accept

--- src/app/add-todo/add-todo.component.ts
+++ src/app/add-todo/add-todo.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit, ViewChild, inject } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
-import { JsonPipe } from '@angular/common';
-import { Todo } from '../todo';
+import { CommonModule, JsonPipe } from '@angular/common';
+import { Todo, TodoStatus } from '../todo';
 import { TodoService } from '../todo.service';
 import Swal from 'sweetalert2'
 import { HttpResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { TodoComponent } from '../todo/todo.component';
 
 @Component({
   selector: 'app-add-todo',
   standalone: true,
-  imports: [FormsModule, JsonPipe],
+  imports: [FormsModule, JsonPipe, CommonModule],
   templateUrl: './add-todo.component.html',
   styleUrl: './add-todo.component.css'
 })
@@ -18,9 +19,11 @@ export class AddTodoComponent {
 
   todoService: TodoService = inject(TodoService);
 
+  todoStatus: { [key: string]: TodoStatus } = TodoStatus;
+
   constructor(private router: Router) {}
 
-  model = new Todo(null, null, null, null, null);
+  model = new Todo(null, null, null, this.todoStatus['SELECT'] , null);
 
   onSubmit(todoForm: NgForm){
     const newTodo = todoForm.value;
@@ -38,6 +41,10 @@ export class AddTodoComponent {
           this.router.navigate([''])
       })
     })
+  }
+
+  getTodoStatus() {
+    return Object.keys(this.todoStatus)
   }
   
 }
--- src/app/todo.ts
+++ src/app/todo.ts
@@ -1,9 +1,18 @@
+export enum TodoStatus {
+    SELECT = "Select",
+    ACTIVE = "Active",
+    IN_PROGRESS = 'In Progress',
+    COMPLETED = 'Completed',
+    PENDING = 'Pending',
+    INACTIVE = 'Inactive',
+}
+
 export class Todo {
     constructor (
         public id: Number | null,
         public title: String | null,
         public details: String | null,
-        public status: String | null,
+        public status: TodoStatus | null,
         public due_date: Date | null, 
     ) {}
 }
