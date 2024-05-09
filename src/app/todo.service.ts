07c310eb44ba4b898ea2afbfdcf7fa3b5db57c2a
KANHAIYA JI <kanhaiyaji14@gmail.com>
2024-05-09 16:49:40 +0530

    Make minor change

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -12,15 +12,15 @@ export class TodoService {
   baseUrl = "http://localhost:3000/todos"
 
   getallTodos(): Observable<Todo[] | []>{
-    return this.http.request<Todo[] | []>('GET', this.baseUrl)
+    return this.http.request<Todo[] | []>('GET', this.baseUrl);
   }
 
   addTodo(todo: Todo): Observable<HttpResponse<Todo>> {
     return this.http.post<Todo>(this.baseUrl, todo, {observe:'response'});
   }
 
-  getTodoDetails(id: Number): Observable<Todo> {
-    return this.http.request<Todo>('GET', `${this.baseUrl}/${id}`) 
+  getTodoDetails(id: number): Observable<Todo> {
+    return this.http.request<Todo>('GET', `${this.baseUrl}/${id}`); 
   }
 
   deleteTodo(id: number): Observable<HttpResponse<any>> {
