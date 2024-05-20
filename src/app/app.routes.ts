2710b99aaf7307fcff9d1db0b9bf1ea81479c330
KANHAIYA JI <kanhaiyaji14@gmail.com>
2024-05-20 19:39:04 +0530

    Add navigation menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { HomeComponent } from './home/home.component';
 import { TodoComponent } from './todo/todo.component';
 import { TodoDetailsComponent } from './todo-details/todo-details.component';
 import { AddTodoComponent } from './add-todo/add-todo.component'; 
+import { TourComponent } from './tour/tour.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
     {path: 'add-todo', component: AddTodoComponent},
     {path: 'todo-details/:id', component: TodoDetailsComponent},
     {path: 'edit-todo/:id', component: AddTodoComponent},
+    {path: 'tour', component: TourComponent},
+    {path: 'about', component: AboutComponent},
     {path: '', component: HomeComponent},
 ];
