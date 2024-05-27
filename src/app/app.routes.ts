6aab933188d1fb3e9622a9fcbe72dd77ba839008
KANHAIYA JI <kanhaiyaji14@gmail.com>
2024-05-27 17:41:31 +0530

    Add content to about us page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { TodoComponent } from './todo/todo.component';
 import { TodoDetailsComponent } from './todo-details/todo-details.component';
 import { AddTodoComponent } from './add-todo/add-todo.component'; 
 import { TourComponent } from './tour/tour.component';
